e39076bba13cb8e30d0d4bb2baaba32fc79942a0
Aparajita Fishman <aparajita@aparajita.com>
2013-10-18 20:38:30 -0400

    Updated grunt infrastructure

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = function(grunt)
+{
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib: {
+                src: ['lib/**/*.js']
+            },
+            test: {
+                src: ['test/**/*.js']
+            },
+        },
+
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib: {
+                files: '<%= jshint.lib.src %>',
+                tasks: ['jshint:lib']
+            },
+            test: {
+                files: '<%= jshint.test.src %>',
+                tasks: ['jshint:test']
+            },
+        },
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['jshint']);
+};

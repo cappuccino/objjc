c02aefb62cad7ec64cdae54fd243a14d46b67976
Aparajita Fishman <aparajita@aparajita.com>
2015-05-15 20:10:56 -0400

    Completed transition to gulp
    Although I will write a gulp plugin for the compiler instead of using a function in the gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+"use strict";
+
+var del = require("del"),
+    gulp = require("gulp"),
+    loadPlugins = require("gulp-load-plugins"),
+    path = require("path");
+
+var plugins = loadPlugins();
+
+// building
+
+gulp.task("clean:build", function(done)
+{
+    del("build/*", done);
+});
+
+gulp.task("clean", gulp.series("clean:build"));
+
+// linting
+
+gulp.task("lint:eslint", function()
+{
+    return gulp.src(["gulpfile.js", "lib/*.js", "test/*.js"])
+        .pipe(plugins.eslint())
+        .pipe(plugins.eslint.format("stylish"));
+});
+
+gulp.task("lint", gulp.series("lint:eslint"));
+
+gulp.task("mocha", function()
+{
+    return gulp.src("test/*.js")
+        .pipe(plugins.mocha({ reporter: "spec" }));
+});
+
+// fixtures
+
+var fixturesPath;
+
+function compileFixture(file, encoding, cb, options)
+{
+    var utils = require("./test/lib/utils");
+
+    console.log(path.relative(fixturesPath, file.path));
+
+    var output = utils.compiledFixture(file.path, options);
+
+    file.contents = new Buffer(options.captureStdout ? output.stdout : output.code);
+
+    if (options.sourceMap)
+    {
+        var fs = require("fs");
+
+        var parsed = path.parse(file.path);
+
+        fs.writeFileSync(path.join(parsed.dir, parsed.name + ".js.map"), output.map);
+    }
+
+    cb(null, file);
+}
+
+gulp.task("generate-fixtures", function()
+{
+    var partialRight = require("lodash/function/partialright"),
+        through = require("through2").obj;
+
+    var normalFilter = plugins.filter("**/{code,formats}/*.j"),
+        warningsFilter = plugins.filter("**/warnings/*.j"),
+        sourceMapsFilter = plugins.filter("**/source-maps/*.j"),
+        cwd = "test/fixtures";
+
+    fixturesPath = path.resolve(cwd);
+
+    // Don't read the files, we only need the paths for the compiler
+    return gulp.src("./**/*.j", { cwd: cwd, read: false })
+        // Compile files that need no special treatment
+        .pipe(normalFilter)
+        .pipe(through(partialRight(compileFixture, {})))
+        .pipe(plugins.rename({ extname: ".js" }))
+        .pipe(gulp.dest(".", { cwd: cwd }))
+        .pipe(normalFilter.restore())
+
+        // Compile warnings, save the warnings as .txt files
+        .pipe(warningsFilter)
+        .pipe(through(partialRight(compileFixture, { captureStdout: true })))
+        .pipe(plugins.rename({ extname: ".txt" }))
+        .pipe(gulp.dest(".", { cwd: cwd }))
+        .pipe(warningsFilter.restore())
+
+        // Compile files that save source maps
+        .pipe(sourceMapsFilter)
+        .pipe(through(partialRight(compileFixture, { sourceMap: true })))
+        .pipe(plugins.rename({ extname: ".js" }))
+        .pipe(gulp.dest(".", { cwd: cwd }));
+});
+
+gulp.task("test", gulp.series("lint", "mocha"));

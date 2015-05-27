0512800b44a6432a37788f36f16343ba9922b45b
Aparajita Fishman <aparajita@aparajita.com>
2015-05-27 12:34:00 +0300

    Only export Runner and reporter

--- index.js
+++ index.js
@@ -10,16 +10,5 @@
 
 "use strict";
 
-exports.cli = require("./lib/cli");
-exports.codeGenerator = require("./lib/code_generator");
-exports.compiler = require("./lib/compiler");
-exports.exceptions = require("./lib/exceptions");
-exports.formats = require("./lib/formats");
-exports.globals = require("./lib/globals");
-exports.Indentation = require("./lib/indentation");
-exports.language = require("./lib/language");
 exports.reporter = require("./lib/reporter");
 exports.Runner = require("./lib/runner");
-exports.Scope = require("./lib/scope");
-exports.StringBuffer = require("./lib/stringbuffer");
-exports.utils = require("./lib/utils");
